a32e72afa5f89dc581f883ae864ec43a5e9a4d87
Will Hansen <will.r.s.hansen@gmail.com>
2019-12-23 02:22:54 -0800

    works with 60s delay and flipped y axis

--- snes_to_xbox.zsh
+++ snes_to_xbox.zsh
@@ -4,11 +4,9 @@
 
 xboxdrv \
   --evdev /dev/input/by-id/usb-0079_USB_Gamepad-event-joystick \
-#  --silent \
-#  --detach-kernel-driver \
-#  --force-feedback \
-#	--deadzone-trigger 15% \
-#	--deadzone 4000 \
+  --detach-kernel-driver \
+	--deadzone-trigger 15% \
+	--deadzone 4000 \
 	--mimic-xpad \
 	--evdev-absmap ABS_X=x1,ABS_Y=y1 \
 	--evdev-keymap BTN_THUMB2=a,BTN_THUMB=b,BTN_TOP=x,BTN_TRIGGER=y,BTN_TOP2=lb,BTN_PINKIE=rb,BTN_BASE3=back,BTN_BASE4=start \
