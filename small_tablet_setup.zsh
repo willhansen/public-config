8689816f89477991b0a007e22dd833a011e2a541
Will Hansen <will.r.s.hansen@gmail.com>
2021-11-27 18:53:48 -0800

    More tablet setup

--- small_tablet_setup.zsh
+++ small_tablet_setup.zsh
@@ -2,7 +2,12 @@ STYLUS=$(xsetwacom --list devices|rg STYLUS|rg -o "id: [0-9]+"|rg -o "[0-9]+")
 PAD=$(xsetwacom --list devices|rg PAD|rg -o "id: [0-9]+"|rg -o "[0-9]+")
 # approximately a rectangle the center of the main monitor, with dimensions of 1/8 of $(xsetwacom --get $DEVICE area)
 # TODO: get size of rectangle programatically, also draw on screen?
-xsetwacom --set "$STYLUS" MapToOutput 1042x585+2300+1300
+AREA=$(xsetwacom --get "$STYLUS" area)
+TABLET_WIDTH=$(echo $AREA | cut -d ' ' -f 3)
+TABLET_HEIGHT=$(echo $AREA | cut -d ' ' -f 4)
+SUBSCREEN_WIDTH=$(($TABLET_WIDTH/8))
+SUBSCREEN_HEIGHT=$(($TABLET_HEIGHT/8))
+xsetwacom --set "$STYLUS" MapToOutput "$SUBSCREEN_WIDTH"x"$SUBSCREEN_HEIGHT"+2300+1300
 # top to bottom, with buttons on left
 xsetwacom --set "$PAD" button 1 "key +ctrl z"
 xsetwacom --set "$PAD" button 2 "key e"
