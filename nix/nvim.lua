f6e5a65662950bef9cdf10fac866c0e188ae04cc
Will Hansen <whansen@ottomotors.com>
2024-08-19 10:12:40 -0400

    disable isort harder

--- nix/nvim.lua
+++ nix/nvim.lua
@@ -373,7 +373,8 @@ lazy.setup({
 		opts = {
 			formatters_by_ft = {
 				-- first use isort and then black
-				python = { "isort", "black" },
+				-- python = { "isort", "black" },
+				python = { "black" },
 				-- "inject" is a special formatter from conform.nvim, which
 				-- formats treesitter-injected code. Basically, this makes
 				-- conform.nvim format python codeblocks inside a markdown file.
