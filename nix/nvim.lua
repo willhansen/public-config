b0e6b870405fd061d0762abf30342e3245209802
Will Hansen <will.r.s.hansen@gmail.com>
2025-02-18 20:11:31 -0800

    trying to add rustowl

--- nix/nvim.lua
+++ nix/nvim.lua
@@ -804,6 +804,13 @@ lazy.setup({
         }
       }
     }
+  },
+  { "cordx56/rustowl", 
+    dependencies = { "neovim/nvim-lspconfig" },
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.rustowlsp.setup({})
+    end,
   }
   -- end of plugins
 })
